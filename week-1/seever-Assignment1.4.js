5ca8323fb22cba96598fd60857afe9a84b9c1058
Jake Seever <jseever@my365.bellevue.edu>
2018-12-02 13:42:34 -0600

    Adding assignment 1.4.

---
+++ week-1/seever-Assignment1.4.js
@@ -0,0 +1,30 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Jake";
+var lastName =  "Seever";
+var age = "35";
+var street = "2045 Timothy Lane";
+var city = "Kronenwetter";
+var state = "Wisconsin";
+var zipCode = "54455";
+
+// output 
+console.log(firstName, lastName, age); 
+console.log(street);
+console.log(city, state, zipCode);
+
+
+// end program 
