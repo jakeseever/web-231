d2055437c811027c2077aa161c0977f1917d77c2
Jake Seever <jseever@my365.bellevue.edu>
2018-12-09 12:41:54 -0600

    Add exercise 2.3

---
+++ week-2/exercise-2.3.js
@@ -0,0 +1,42 @@
+/*
+============================================
+; Title:  seever-header-test.js
+; Author: Jake Seever
+; Date:   09 December 2018
+; Description: Displays a formatted header
+;===========================================
+*/
+
+var header = require('../seever-header.js');
+
+// pass in the firstName, lastName, and assignment parameters required for the header.
+console.log(header.display("Jake", "Seever", "Exercise 2.3 - Function Properties"));
+
+// start program
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 2.3
+  Today's Date
+
+  Hello FirstName LastName!
+
+  Hint: Use your personal header display function and
+  refer to page 178 for implementing function properties
+*/
+
+// function properties go here...
+myName.jake = "Jake";
+
+// function
+function myName() {
+  return myName.jake;
+}
+
+// output
+console.log('\n');
+console.log("Hello " + myName() + " Seever!");
+
+// end program
